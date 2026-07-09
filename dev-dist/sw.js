7f12ae27b9b877d799307d1a2a320685176b70fe
cauejeKreutz <cauejoaquimlk@gmail.com>
2026-07-09 11:32:43 -0300

    feat: update MotorComponent and PaisesComponent with revised content and responsive styles

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-5d155c7a'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1gkmfi1hjc8"
+    "revision": "0.2tnl9ohgomg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
